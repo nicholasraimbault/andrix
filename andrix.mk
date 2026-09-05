598287cb51a1bbdcf473c947d93f54c6dc18b07c
nicholasraimbault <11843674+nicholasraimbault@users.noreply.github.com>
2026-09-04 21:52:33 -0500

    Allow the built-in usr mountpoint artifact

--- andrix.mk
+++ andrix.mk
@@ -8,3 +8,8 @@ PRODUCT_PACKAGES += \
 # /usr exist in the image before init bind-mounts the APEX over it.
 PRODUCT_COPY_FILES += \
     vendor/andrix/init/usr.mountpoint:$(TARGET_COPY_OUT_ROOT)/usr/.andrix-mountpoint
+
+# generic_system owns root's artifact paths. Permit only this Andrix mountpoint
+# marker, without disabling or broadly relaxing AOSP's path enforcement.
+PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
+    $(TARGET_COPY_OUT_ROOT)/usr/.andrix-mountpoint
